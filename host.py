733eec67432a42744122139cd2c5f388702dd216
Divyansh Singhal <dsinghal250802@gmail.com>
2022-04-05 23:51:00 +0530

    Testing API Part-2

--- host.py
+++ host.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api=Api(app)
 
 @app.route('/')
 def index():
@@ -10,16 +12,30 @@ def index():
 def myapp():
     return '<h1>The app page</h1>'
 
-@app.route('/api')
-def api():
-    return '<h1>The API Thing here</h1>'
+class Hello(Resource):
+    def get(self):
+        return jsonify({'message':'hello world'})
+
+    def post(self):
+        data=request.get_json()
+        return jsonify({'data: data'})
+
+class Square(Resource):
+    def get(self,num):
+        return jsonify({'square: num**2'})
+
+api.add_resource(Hello,'/')
+api.add_resource(Square, '/square/<int:num>')
+# @app.route('/api')
+# def api():
+#     return '<h1>The API Thing here</h1>'
 
 
-@app.route('/post', methods=["POST"])
-def testpost():
-     input_json = request.get_json(force=True) 
-     dictToReturn = {'text':input_json['text']}
-     return jsonify(dictToReturn)
+# @app.route('/post', methods=["POST"])
+# def testpost():
+#      input_json = request.get_json(force=True) 
+#      c = {'sum':int(input_json['a']) + int(input_json['b']) }
+#      return jsonify(c)
 
 
 if __name__=='__main__':
