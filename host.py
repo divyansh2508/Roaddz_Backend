0cdd463df09a78d92afdb0bc28704a1df5a298a0
Divyansh Singhal <dsinghal250802@gmail.com>
2022-04-05 22:40:14 +0530

    Testing API Part-1

--- host.py
+++ host.py
@@ -15,5 +15,12 @@ def api():
     return '<h1>The API Thing here</h1>'
 
 
+@app.route('/post', methods=["POST"])
+def testpost():
+     input_json = request.get_json(force=True) 
+     dictToReturn = {'text':input_json['text']}
+     return jsonify(dictToReturn)
+
+
 if __name__=='__main__':
     app.run(debug=True)
